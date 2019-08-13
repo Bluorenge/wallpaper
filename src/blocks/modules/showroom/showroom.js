7180ebcab3e17f960c5146173d7a294e773dbff9
Anty <zerkacher@gmail.com>
2019-08-13 15:47:46 +0400

    Upload files site

---
+++ src/blocks/modules/showroom/showroom.js
@@ -0,0 +1,13 @@
+modules.define('showroom', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
